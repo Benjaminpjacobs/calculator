32281dc668c67f6fc5f8f1e3e2c4e4a52b702552
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-03-21 13:18:20 -0600

    create calculator with test

---
+++ lib/calculator.rb
@@ -0,0 +1,45 @@
+require 'pry'
+class Calculator
+  attr_accessor :total
+
+  def initialize
+    @total = 0
+  end
+
+  def update_total(number, operation)
+    case operation
+    when "add_two"
+      @total = number
+    when "clear"
+      @total = number
+    when "subtract_two"
+      @total = number
+    when "add"
+      @total += number
+    when "subtract"
+      @total -= number
+    end
+  end
+
+  def add(num1, *num2)
+    if !num2.empty?
+      sub_total = num1 + num2.join.to_i
+      update_total(sub_total, "add_two")
+    else
+      update_total(num1, "add")
+    end
+  end 
+  
+  def clear
+    update_total(0,"clear")
+  end
+
+  def subtract(num1, *num2)
+    if !num2.empty?
+      sub_total = num1 - num2.join.to_i
+      update_total(sub_total, "subtract_two")
+    else
+      update_total(num1, "subtract")
+    end
+  end
+end
---
+++ test/calculator_test.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/calculator'
+require 'pry'
+
+
+class CalculatorTest < Minitest::Test
+  def test_it_exits
+    calc = Calculator.new
+    assert_instance_of Calculator, calc
+  end
+  def test_it_starts_with_zero_total
+    calc = Calculator.new
+    assert_equal 0, calc.total
+  end
+  def test_it_can_add
+    calc = Calculator.new
+    calc.add(2,2)
+    assert_equal 4, calc.total
+  end
+  def test_it_can_clear
+    calc = Calculator.new
+    calc.add(2,2)
+    calc.clear
+    assert_equal 0, calc.total
+  end
+  def test_it_can_subtract
+    calc = Calculator.new
+    calc.subtract(10,2)
+    assert_equal 8, calc.total
+  end
+  def test_add_can_take_multiple_args
+    calc = Calculator.new
+    calc.add(10)
+    assert_equal 10, calc.total
+  end
+  def test_subtract_can_take_multiple_args
+    calc = Calculator.new
+    calc.subtract(10)
+    assert_equal -10, calc.total
+  end
+  def test_add_to_total
+    calc = Calculator.new
+    calc.add(2,4)
+    calc.add(2)
+    assert_equal 8, calc.total
+  end
+  def test_subtract_from_total
+    calc = Calculator.new
+    calc.add(2,4)
+    calc.subtract(5)
+    assert_equal 1, calc.total
+  end
+end
